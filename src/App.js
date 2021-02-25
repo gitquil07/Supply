92dc29dbd881236bb209cfbb70e409a04c4060ba
yakohere <yakhyoismoildjonov@gmail.com>
2021-02-25 12:24:25 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
-import { makeStyles, } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { Sidebar } from './components/Sidebar';
@@ -11,25 +8,16 @@ import styled from "styled-components";
 import { User } from './components/Sidebar/user';
 import Background from "./assets/bg.jpg";
 
-const useStyles = makeStyles(() => ({
-  drawerPaper: {
-    width: "270px",
-  },
-}));
-
 const App = () => {
-  const classes = useStyles();
   const [drawer, setDrawer] = useState(true);
 
   return (
     <Wrapper>
-      <StyledAppBar position="fixed" open={drawer}>
-        <Toolbar style={{ padding: "0 10px" }}>
-          <IconButton onClick={() => setDrawer(!drawer)}>
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-      </StyledAppBar>
+      <Header open={drawer}>
+        <IconButton onClick={() => setDrawer(!drawer)}>
+          <MenuIcon />
+        </IconButton>
+      </Header>
 
       <StyledDrawer variant="persistent" anchor="left" open={drawer}>
         <User />
@@ -46,17 +34,27 @@ const App = () => {
 export default App;
 
 
-const Wrapper = styled.div``;
-
-const Main = styled.div`
-  background-color: red;
-  margin-top: 80px;
-  margin-left: ${props => props.open ? "290px" : "20px"};
+const Wrapper = styled.div`
+  width: 100%;
 `;
 
-const StyledAppBar = styled(AppBar)`
-  background-color: #fff;
+const Main = styled.div` 
+  margin-top: 10px;
+  margin-left: ${props => props.open ? "290px" : "20px"};
+  padding-right: 20px;
+`;
+
+const Header = styled.div`
   width: ${props => props.open && "calc(100% - 270px)"};
+  margin-left: ${props => props.open && "270px"};
+  height: 60px;
+  background-color: #fff;
+  box-shadow: -1px 1px 8px 0px rgba(67,67,67,0.66);
+  -webkit-box-shadow: -1px 1px 8px 0px rgba(67,67,67,0.66);
+  -moz-box-shadow: -1px 1px 8px 0px rgba(67,67,67,0.66); 
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 const StyledDrawer = styled(Drawer)`
---
+++ src/components/Title/index.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Title = ({ name }) => <Wrapper>{name}</Wrapper>
+
+const Wrapper = styled.h1`
+    color: #474747;
+`;
